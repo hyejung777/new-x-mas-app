7ccc37cfae98da7d56ee929e93a3aa2e07e8673d
hyejung <lstarsl1214@naver.com>
2023-12-21 18:45:30 +0900

    해피버스테이

---
+++ lib/23_12_20/stop_watch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class stopWatch extends StatefulWidget {
+  const stopWatch({super.key});
+
+  @override
+  State<stopWatch> createState() => _stopWatchState();
+}
+
+class _stopWatchState extends State<stopWatch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
